ac9e013029d179dff412d5602d7bed5f4a711399
unknown <wzha@wisc.edu>
2019-08-20 22:47:02 -0500

    added test_2

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # W. Zha @ 08/20/2019
 # 
-python3.7 ./src/border_analytics.py ./input/Border_Crossing_Entry_data.csv ./output/report.csv
+python3.7 ./src/border_analytics.py ./input/Border_Crossing_Entry_Data.csv ./output/report.csv
